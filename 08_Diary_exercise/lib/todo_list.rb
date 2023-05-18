7d5e62d3d6cbf7da601aaf5a5d8391405f46735c
Shoaib Latif <shoaiblatif11@gmail.com>
2023-05-18 16:58:33 +0100

    Added in methods and added in first integration test

--- 08_Diary_exercise/lib/todo_list.rb
+++ 08_Diary_exercise/lib/todo_list.rb
@@ -9,12 +9,15 @@ class TodoList
   def incomplete
     # Returns all non-done todos
   end
+  
 
   def complete
     # Returns all complete todos
+    
   end
 
   def give_up!
     # Marks all todos as complete
+
   end
 end
--- 08_Diary_exercise/spec/todo_integration.rb
+++ 08_Diary_exercise/spec/todo_integration.rb
@@ -2,5 +2,12 @@ require "todo"
 require "todo_list"
 
 RSpec.describe "integration" do
-  
+  context "adding incomplete entries" do
+    it "Lists one entry after being added" do
+      todo = Todo.new
+      todo_list = TodoList.new("Buy Chocolate")
+      todo_list.add(todo)
+      expect(diary.incomplete).to eq [todo]
+    end
+  end
 end
